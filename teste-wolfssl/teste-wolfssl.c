76ac64e859568919e8c0085336e92db213e8d5f7
ulyssesfonseca <ulysses-2010@hotmail.com>
2020-01-09 16:47:11 -0300

    now work well the connection with tago.io tls

--- teste-wolfssl/teste-wolfssl.c
+++ teste-wolfssl/teste-wolfssl.c
@@ -4,18 +4,48 @@
 #include <errno.h>
 #include <stdio.h>
 
-#define LOCAL_TEST
-#define USE_FILE_CERT "tago_cert.pem"
+//#define LOCAL_TEST
+//#define USE_FILE_CERT "tago_cert.pem"
+//#define USE_FILE_CERT "ca-cert.pem"
+//#define USE_FILE_CERT "sf-class2-root.crt"
+
 
 
 #ifndef LOCAL_TEST
-#define SERV_PORT 443//1500//1111
+#define SERV_PORT 443
+#define SERVER_ADDR "23.22.53.220"
 #else
 #define SERV_PORT 1500
+#define SERVER_ADDR "192.168.222.192"
 #endif
 
 const unsigned char tago_cert[] = {
-		"-----BEGIN CERTIFICATE-----\n"
+	"-----BEGIN CERTIFICATE-----\n"
+"MIIEDzCCAvegAwIBAgIBADANBgkqhkiG9w0BAQUFADBoMQswCQYDVQQGEwJVUzEl\n"
+"MCMGA1UEChMcU3RhcmZpZWxkIFRlY2hub2xvZ2llcywgSW5jLjEyMDAGA1UECxMp\n"
+"U3RhcmZpZWxkIENsYXNzIDIgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMDQw\n"
+"NjI5MTczOTE2WhcNMzQwNjI5MTczOTE2WjBoMQswCQYDVQQGEwJVUzElMCMGA1UE\n"
+"ChMcU3RhcmZpZWxkIFRlY2hub2xvZ2llcywgSW5jLjEyMDAGA1UECxMpU3RhcmZp\n"
+"ZWxkIENsYXNzIDIgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwggEgMA0GCSqGSIb3\n"
+"DQEBAQUAA4IBDQAwggEIAoIBAQC3Msj+6XGmBIWtDBFk385N78gDGIc/oav7PKaf\n"
+"8MOh2tTYbitTkPskpD6E8J7oX+zlJ0T1KKY/e97gKvDIr1MvnsoFAZMej2YcOadN\n"
+"+lq2cwQlZut3f+dZxkqZJRRU6ybH838Z1TBwj6+wRir/resp7defqgSHo9T5iaU0\n"
+"X9tDkYI22WY8sbi5gv2cOj4QyDvvBmVmepsZGD3/cVE8MC5fvj13c7JdBmzDI1aa\n"
+"K4UmkhynArPkPw2vCHmCuDY96pzTNbO8acr1zJ3o/WSNF4Azbl5KXZnJHoe0nRrA\n"
+"1W4TNSNe35tfPe/W93bC6j67eA0cQmdrBNj41tpvi/JEoAGrAgEDo4HFMIHCMB0G\n"
+"A1UdDgQWBBS/X7fRzt0fhvRbVazc1xDCDqmI5zCBkgYDVR0jBIGKMIGHgBS/X7fR\n"
+"zt0fhvRbVazc1xDCDqmI56FspGowaDELMAkGA1UEBhMCVVMxJTAjBgNVBAoTHFN0\n"
+"YXJmaWVsZCBUZWNobm9sb2dpZXMsIEluYy4xMjAwBgNVBAsTKVN0YXJmaWVsZCBD\n"
+"bGFzcyAyIENlcnRpZmljYXRpb24gQXV0aG9yaXR5ggEAMAwGA1UdEwQFMAMBAf8w\n"
+"DQYJKoZIhvcNAQEFBQADggEBAAWdP4id0ckaVaGsafPzWdqbAYcaT1epoXkJKtv3\n"
+"L7IezMdeatiDh6GX70k1PncGQVhiv45YuApnP+yz3SFmH8lU+nLMPUxA2IGvd56D\n"
+"eruix/U0F47ZEUD0/CwqTRV/p2JdLiXTAAsgGh1o+Re49L2L7ShZ3U0WixeDyLJl\n"
+"xy16paq8U4Zt3VekyvggQQto8PT7dL5WXXp59fkdheMtlb71cZBDzI0fmgAKhynp\n"
+"VSJYACPq4xJDKVtHCN2MQWplBqjlIapBtJUhlbl90TSrE9atvNziPTnNvT51cKEY\n"
+"WQPJIrSPnNVeKtelttQKbfi3QBFGmh95DmK/D5fs4C8fF5Q=\n"
+"-----END CERTIFICATE-----\n"
+};
+/*		"-----BEGIN CERTIFICATE-----\n"
 		"MIIEojCCA4qgAwIBAgITBn+UV0u3B10+SJZceDIkqnVP7TANBgkqhkiG9w0BAQsF\n"
 		"ADCBmDELMAkGA1UEBhMCVVMxEDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNj\n"
 		"b3R0c2RhbGUxJTAjBgNVBAoTHFN0YXJmaWVsZCBUZWNobm9sb2dpZXMsIEluYy4x\n"
@@ -42,7 +72,7 @@ const unsigned char tago_cert[] = {
 		"RZ4FeoPETzXfKd98y0l9v6lx2guQ4hNA3vCYDMF+w/2TcVW6YPVMjEkuRN2hypcY\n"
 		"uNmaqaMA0K0ZD7nQOedXceNRfr+GruHRduh2EZI0gMxfOpwfn98=\n"
 		"-----END CERTIFICATE-----\n"
-};
+};*/
 int sockfd;
 
 int cbk_recv(WOLFSSL *ssl, char *buf, int sz, void *ctx){
@@ -63,24 +93,21 @@ int main()
 	WOLFSSL* ssl;
 	WOLFSSL_METHOD* method;
 	struct  sockaddr_in servAddr;
+	char buf_rec[512];
 	const char message[] = 
 		"POST /data HTTP/1.1\r\n"
 		"Host: api.tago.io\r\n"
 		"content-type: application/json\r\n"
 		"content-length: 34\r\n"
 		"device-token: c7f71928-9510-4cda-9c69-29a0e35d44b9\r\n\r\n"
-		"{\"variable\":\"board\";\"value\":\"375\"}";
+		"{\"variable\":\"board\",\"value\":\"375\"}";
 
 	/* create and set up socket */
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&servAddr, 0, sizeof(servAddr));
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_port = htons(SERV_PORT);
-#ifndef LOCAL_TEST
-	servAddr.sin_addr.s_addr = inet_addr("api.tago.io");
-#else
-	servAddr.sin_addr.s_addr = inet_addr("192.168.222.192");
-#endif
+	servAddr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
 
 	printf("Connect to socket\n");
     /* connect to socket */
@@ -122,7 +149,10 @@ int main()
 	printf("wolfssl_connect \n");
 	printf("return connect:%d\n",wolfSSL_connect(ssl));
 	printf("wolfssl_write \n");
-	wolfSSL_write(ssl, message, strlen(message));
+	printf("-->write: %d \n",wolfSSL_write(ssl, message, strlen(message)));
+
+	printf("-->read: %d \n", wolfSSL_read(ssl, buf_rec, sizeof(buf_rec)));
+	printf("received:'%s'\n",buf_rec);
 
     /* frees all data before client termination */
 	printf("wolfssl_free \n");
